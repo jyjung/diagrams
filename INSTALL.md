0f304b42194e1a11588e02227334287d3a678697
Jung Jinyoung <bungker@gmail.com>
2024-11-08 18:05:58 +0900

    update

--- INSTALL.md
+++ INSTALL.md
@@ -1,2 +1,2 @@
-pip install git+https://github.com/jyjung/diagrams@master#egg=diagrams
+pip install git+https://github.com/jyjung/diagrams@master#egg=diagrams-security365
 pip install git+https://github.com/jyjung/diagrams@master#egg=resources
--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ setup(
     name='diagrams-security365',
     version='0.1',
     packages=find_packages(),
+    include_package_data=True,  # 추가
+    package_data={
+        '': ['resources/*'],  # 프로젝트의 리소스를 포함
+    },    
     install_requires=[],
     author='Jung Jin Young',
     author_email='bungker@gmail.com',
