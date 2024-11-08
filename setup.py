38d4d623b0b13009cd9e4e8a805e09216f3b4ab5
Jung Jinyoung <bungker@gmail.com>
2024-11-08 17:18:35 +0900

    feat: add setup.py for diagrams package with initial configuration

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='diagrams',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='Jung Jin Young',
+    author_email='bungker@gmail.com',
+    description='Diagrams extend for  Security365',
+    url='https://github.com/jyjung/diagrams',
+)
