1e3d3145d4c708dd295cadebf2826d4a9337e284
Jung Jinyoung <bungker@gmail.com>
2024-11-08 18:12:10 +0900

    update

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup, find_packages
+import os
+
+resource_files = [
+    os.path.join('resources', f) for f in os.listdir('resources')
+]
 
 setup(
     name='diagrams-security365',
     version='0.1',
     packages=find_packages(),
-    include_package_data=True,  # 추가
+    include_package_data=True,
     package_data={
-        '': ['resources/*'],  # 프로젝트의 리소스를 포함
-    },    
+        'my_package': [],  # 패키지 내에 데이터 파일을 포함하지 않음
+    },
+    data_files=[('resources', resource_files)],  # /site-packages에 복사할 파일    
     install_requires=[],
     author='Jung Jin Young',
     author_email='bungker@gmail.com',
